a1ae54c24c55a1849cae58c543e9fda6d746704e
kmvbatista <kennedymnovas@gmail.com>
2019-10-17 16:08:48 -0300

    first commit \

---
+++ Model.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Domain.Entities
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string BirthDate { get; set; }
+        public int Cpf { get; set; }
+    }
+}
